f86871720e295ec1f1fa5e646e26c6967a84128e
Shadman Ahmed <shadman@outlook.com>
2020-05-14 22:34:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,7 +29,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = ["am","am","am",4]
+  array = ["am", "am", "am", 4]
   array.index(index_number)
 end
 
