7489768d9bab052ccd2f5fd4070ac10cf5be689e
Shadman Ahmed <shadman@outlook.com>
2020-05-14 21:28:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,12 +8,11 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = [1,2,3,4]
-  array.push[5]
+  array.push[element]
 end
 
 def add_element_to_start_of_array(array, element)
   array = [1,2,3,4]
-
 end
 
 def remove_element_from_end_of_array(array)
